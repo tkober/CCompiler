ab282d532d51e185f7e405a1181abb28c1e741b6
Thorsten Kober <Kober@semvox.de>
2014-05-08 11:48:45 +0200

    Debugging hinzugefügt

---
+++ CompilerConstruction/CCDebug.h
@@ -0,0 +1,16 @@
+//
+//  CCDebug.h
+//  CompilerConstruction
+//
+//  Created by Thorsten Kober on 08.05.14.
+//  Copyright (c) 2014 Thorsten Kober. All rights reserved.
+//
+
+#ifndef CompilerConstruction_CCDebug_h
+#define CompilerConstruction_CCDebug_h
+
+
+#define BISON_DEBUG 0
+
+
+#endif
